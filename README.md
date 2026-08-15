4b9d25721c7d1a9ebaaedb6fb6f7be242496a414
Sudip Roy <sudiproy1999@gmail.com>
2026-08-16 01:03:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo has a sample setup for c++ project on Linux.
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b);
+
+int main(){
+    int a = 8, b = 2;
+    cout<<sum(a, b);
+}
