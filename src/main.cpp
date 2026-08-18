e5cbe6922a48048e16b157fa4fb5bb5253ccbcc6
Sudip Roy <sudiproy1999@gmail.com>
2026-08-19 00:29:11 +0530

    add current directory path

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,27 @@
 #include <vector>
 #include "inputParser.h"
 #include "execute.h"
+#include <filesystem>
 
 using namespace std;
+
+string getPathFromHome() {
+    string str = filesystem::current_path();
+    return str.substr(str.find_last_of("/"));
+}
+
 int main(){
     while(true) {
-        printf(" $ ");
+
+        //Show current directory
+        cout << "[~" << getPathFromHome() << "]$ ";
+
+        //take input
         string input;
         getline(cin, input);
 
         vector<string> parsedInput = parser(input);
         cout << executeCommand(parsedInput) << endl;
     }
+    return 0;
 }
